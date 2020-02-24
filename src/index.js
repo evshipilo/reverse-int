ade1eeb2e08f88435373a5a04708e1bf49e75c3e
Yauheni Shypila <evshipilo@gmail.com>
2020-02-24 16:25:10 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let arr=n.toString().split('').reverse();
+  while(true){
+      if(arr[0]==0) arr.splice(0,1);
+  }
+    if(arr[arr.length-1]=='-') arr.splice(arr.length-1,1);
+
+ return arr.join('');
+
 }
